85bb4882b1f9016b33401d6b5ccc9a86f81f6df3
Rillien65 <biancat65@yahoo.com.au>
2025-10-03 01:14:28 +1000

    Create Firebase-config.js
    Updates

---
+++ Firebase-config.js
@@ -0,0 +1,11 @@
+// Replace with your Firebase config
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "your-project.firebaseapp.com",
+  projectId: "your-project-id",
+  storageBucket: "your-project.appspot.com",
+  messagingSenderId: "your-sender-id",
+  appId: "your-app-id"
+};
+
+firebase.initializeApp(firebaseConfig);
